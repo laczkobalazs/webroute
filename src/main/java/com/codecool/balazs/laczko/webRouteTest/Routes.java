476df90c37337b1f0dfa60bc4695f93075a1492c
laczkobalazs <laczko.balazs89@gmail.com>
2020-07-24 21:07:29 +0200

    adds Routes class with methods and WebRoute annotation

---
+++ src/main/java/com/codecool/balazs/laczko/webRouteTest/Routes.java
@@ -0,0 +1,11 @@
+package com.codecool.balazs.laczko.webRouteTest;
+
+public class Routes {
+    public String test1() {
+        return "Hello from test1";
+    }
+
+    public String test2() {
+        return "Hello from test2 as well";
+    }
+}
