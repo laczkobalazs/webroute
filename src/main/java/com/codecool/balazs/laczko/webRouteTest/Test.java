4e8d83e9ffb2cce47423da45b200b102e7de24ba
laczkobalazs <laczko.balazs89@gmail.com>
2020-07-24 21:00:42 +0200

    adds AnotherMyHandle for a different pathing with ~/another

--- src/main/java/com/codecool/balazs/laczko/webRouteTest/Test.java
+++ src/main/java/com/codecool/balazs/laczko/webRouteTest/Test.java
@@ -12,6 +12,7 @@ public class Test {
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
         server.createContext("/test", new MyHandler());
+        server.createContext("/another", new AnotherMyHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
     }
@@ -23,6 +24,17 @@ public class Test {
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+    static class AnotherMyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "This is the response from /another with AnotherMyHandle";
+            t.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream os = t.getResponseBody();
+            os.write((response.getBytes()));
             os.close();
         }
     }
